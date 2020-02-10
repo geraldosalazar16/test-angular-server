11308af06b8ea657e1e0c7febea75a37c8193596
geraldosalazar16 <geraldosalazar16@gmail.com>
2020-02-09 22:22:33 -0600

    Ajustes CORS

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const https = require('https')
 
 var app = express();
+
+app.use(cors())
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,15 +21,35 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.get('/data', (request, response) => {
+
+  const req = https.get('https://feeds.citibikenyc.com/stations/stations.json', res => {
+
+    let data = '';
+    res.on('data', d => {
+      data += d;
+    });
+
+    // The whole response has been received. Print out the result.
+    res.on('end', () => {
+      response.json(JSON.parse(data));
+    });
+  })
+
+  req.on('error', error => {
+    response.status(500).send(error);
+  })
+
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
